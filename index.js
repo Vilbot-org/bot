16653889a054ea3b3d6e311d3ce95c3274f72d09
GFrancV <gfrancv@hotmail.com>
2022-09-28 11:22:59 +0100

    Add basic test command (test).

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 
 //Enviroment variables
 const dotenv = require("dotenv");
@@ -7,11 +7,35 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const intents = [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMembers,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+];
+
+const client = new Client({
+	intents: intents,
+});
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
 	console.log(`Bot listo como: ${client.user.tag}`);
+});
+
+//To read commands
+client.on("messageCreate", async message => {
+	if (message.author.bot || !message.guild) return;
+
+	const command = message.content;
+
+	if (command == "ping") {
+		const embed = new EmbedBuilder()
+			.setTitle("Pong")
+			.setDescription(`Ping es de ${client.ws.ping}ms`)
+			.setColor("02fef7");
+		message.channel.send({ embeds: [embed] });
+	}
 });
 
 client.on("interactionCreate", async interaction => {
