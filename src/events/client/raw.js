d027908b69bef449ea2fb8ce66ca70bea8c7a17e
GFrancV <gfrancv@hotmail.com>
2022-09-29 17:15:42 +0100

    Checkpoint.

---
+++ src/events/client/raw.js
@@ -0,0 +1,11 @@
+const Event = require("../../structures/Event");
+
+module.exports = class extends Event {
+	constructor(client) {
+		super(client, { name: "raw" });
+	}
+
+	run = async d => {
+		await this.client.manager.updateVoiceState(d);
+	};
+};
