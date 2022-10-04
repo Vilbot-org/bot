45bd542c526a5f304870b6886e6217ddd15ffa21
GFrancV <gfrancv@hotmail.com>
2022-10-04 11:56:17 +0100

    Only music commands in the bot channel.

---
+++ src/functions/exclusiveChannel.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder } = require("discord.js");
+
+const { colors } = require("../../config.json");
+
+module.exports = async (client, interaction) => {
+	//Notify to the user and delete the message
+	const interactionReply = await interaction.reply({
+		embeds: [
+			new EmbedBuilder()
+				.setColor(colors.danger)
+				.setTitle(":x: You can't send that message on this channel!")
+				.setDescription(
+					"This channel is exclusive for music commands, use another channel to send messages!"
+				),
+		],
+	});
+	await interaction.delete();
+
+	setTimeout(() => {
+		interactionReply.delete();
+	}, 6000);
+};
