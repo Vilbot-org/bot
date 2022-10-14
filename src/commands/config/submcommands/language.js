3cc9995c2a4f32082fee1149ea7e66e20b4ec476
GFrancV <gfrancv@hotmail.com>
2022-10-14 16:58:48 +0100

    Multilanguage ready (need to translate all the messages).

--- src/commands/config/submcommands/language.js
+++ src/commands/config/submcommands/language.js
@@ -1,9 +1,32 @@
+const { EmbedBuilder } = require("discord.js");
+
+const { colors } = require("../../../config.json");
+const { setLanguage } = require("../../../structures/Language");
+
+const language = require("../../../structures/Language");
+
 module.exports = async (client, interaction, snipe) => {
 	const languageToSet = interaction.options.getString("language");
 
-	/* const guildData = await snipe.findById(interaction.guildId);
+	let guildData = await snipe.findOneAndUpdate({ _id: interaction.guild.id }, { language: languageToSet });
 
 	if (!guildData) {
-		console.log("no existo");
-	} */
+		guildData = await new snipe({
+			_id: interaction.guild.id,
+			language: languageToSet,
+		});
+		await guildData.save();
+	}
+
+	setLanguage(interaction.guild.id, languageToSet);
+
+	return await interaction.reply({
+		embeds: [
+			new EmbedBuilder()
+				.setColor(colors.green)
+				.setAuthor({ name: language(interaction.guild.id, "config.language.success") })
+				.setTitle(language(interaction.guild.id, "config.language.set", languageToSet.toUpperCase()))
+				.setThumbnail(interaction.guild.iconURL()),
+		],
+	});
 };
