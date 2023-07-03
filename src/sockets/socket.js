6dfe6339429062d86f77d531e7edd6f02ff57fce
GFrancV <gfrancv@hotmail.com>
2023-07-03 11:53:50 +0100

    feat(socketIO): Add auth with JWT to auth the connection.

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,5 +1,25 @@
 import io from 'socket.io-client';
+import jwt from 'jsonwebtoken';
+import { useMasterPlayer as player } from 'discord-player';
 
-const socket = io('http://localhost:3000/');
+const token = jwt.sign({ user: 'Vilbot' }, 'thisisasecretkey');
+
+const socket = io('http://localhost:3000/', {
+	auth: {
+		token
+	}
+});
+
+socket.on('bot.playMusic', async (data) => {
+	const searchResult = await player().search(data);
+
+	const { track } = await player().play('1026501495513952349', searchResult, {
+		nodeOptions: {
+			volume: 40
+		}
+	});
+
+	console.log(track.title);
+});
 
 export default socket;
