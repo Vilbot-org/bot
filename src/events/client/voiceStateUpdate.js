8071f0781c55c81349cee593898f1b5daf04a11a
GFrancV <gfrancv@hotmail.com>
2023-09-22 10:31:21 +0100

    fix(client-events): In voice state update event if the user is the bot dont register the activity.

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,3 +1,4 @@
+import client from '@/Client';
 import Guild from '@/models/Guild';
 import { Events } from 'discord.js';
 
@@ -9,29 +10,31 @@ export default {
 		const { channelId: oldChannelID } = oldState;
 		const { channelId: newChannelID, id: userId } = newState;
 
-		const guild = await Guild.findById(guildId);
+		if (userId !== client.user.id) {
+			const guild = await Guild.findById(guildId);
 
-		const activeVoiceUsers = JSON.parse(guild.activeVoiceUsers) || {};
+			const activeVoiceUsers = JSON.parse(guild.activeVoiceUsers) || {};
 
-		if (oldChannelID && activeVoiceUsers[oldChannelID] !== undefined) {
-			activeVoiceUsers[oldChannelID] = activeVoiceUsers[oldChannelID].filter(
-				(user) => user !== userId
-			);
+			if (oldChannelID && activeVoiceUsers[oldChannelID] !== undefined) {
+				activeVoiceUsers[oldChannelID] = activeVoiceUsers[oldChannelID].filter(
+					(user) => user !== userId
+				);
 
-			if (activeVoiceUsers[oldChannelID].length === 0) {
-				delete activeVoiceUsers[oldChannelID];
+				if (activeVoiceUsers[oldChannelID].length === 0) {
+					delete activeVoiceUsers[oldChannelID];
+				}
 			}
-		}
 
-		if (newChannelID) {
-			if (activeVoiceUsers[newChannelID] === undefined) {
-				activeVoiceUsers[newChannelID] = [];
+			if (newChannelID) {
+				if (activeVoiceUsers[newChannelID] === undefined) {
+					activeVoiceUsers[newChannelID] = [];
+				}
+				activeVoiceUsers[newChannelID].push(userId);
 			}
-			activeVoiceUsers[newChannelID].push(userId);
+
+			guild.activeVoiceUsers = JSON.stringify(activeVoiceUsers);
+
+			guild.save();
 		}
-
-		guild.activeVoiceUsers = JSON.stringify(activeVoiceUsers);
-
-		guild.save();
 	}
 };
