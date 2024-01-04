5311e8a5130184988108555f68af4a8e49fd9f58
GFrancV <gfrancv@hotmail.com>
2024-01-04 09:19:36 +0000

    build: Add verification if the Vilbot container exist and remove it

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,13 @@ docker rm $CONTAINER_NAME
 
 # Deploy container
 docker build -t Vilbot -f dockerfile.production .
+
+if [ "$(docker ps -a | grep -c $CONTAINER)" -gt 0 ]; then
+  docker rm -f Vilbot
+fi
+
 docker run Vilbot
 
 rm -R dist/
+
+exit
