c3a949471807e12551411ee9e94a61267613fc76
GFrancV <gfrancv@hotmail.com>
2024-01-08 12:42:02 +0000

    Add lint, build and deploy steps to workflow for test

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-CONTAINER_NAME="vilbot-transpiler-container"
+# CONTAINER_NAME="vilbot-transpiler-container"
 
-# Transpiler contianer
-docker build -t vilbot-transpiler .
-mkdir dist
-docker run --name $CONTAINER_NAME vilbot-transpiler
-docker cp $CONTAINER_NAME:/app/dist dist/
-docker rm $CONTAINER_NAME
+# # Transpiler contianer
+# docker build -t vilbot-transpiler .
+# mkdir dist
+# docker run --name $CONTAINER_NAME vilbot-transpiler
+# docker cp $CONTAINER_NAME:/app/dist dist/
+# docker rm $CONTAINER_NAME
 
 # Deploy container
-docker build --no-cache -t vilbot -f dockerfile.production .
+docker build --no-cache -t vilbot -f dockerfile .
 
 if docker inspect -f '{{.Name}}' vilbot 2>/dev/null; then
 	if docker inspect -f '{{.State.Running}}' vilbot 2>/dev/null; then
@@ -20,7 +20,5 @@ if docker inspect -f '{{.Name}}' vilbot 2>/dev/null; then
 fi
 
 docker run --name vilbot -d --restart always vilbot
-
-rm -R dist/
 
 exit
