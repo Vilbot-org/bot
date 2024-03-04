0f1bd676bc6a1216db33d0927099102f769c2da5
GFrancV <gfrancv@hotmail.com>
2024-03-04 11:14:59 +0000

    test: Add testing with jest to the project

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {};
+
+module.exports = config;
