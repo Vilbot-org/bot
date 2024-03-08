6e3e6d657a1644f7a37016e63e04853c971d717f
GFrancV <gfrancv@hotmail.com>
2024-03-08 10:31:12 +0000

    build: Update new deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-# CONTAINER_NAME="vilbot-transpiler-container"
-
-# # Transpiler contianer
-# docker build -t vilbot-transpiler .
-# mkdir dist
-# docker run --name $CONTAINER_NAME vilbot-transpiler
-# docker cp $CONTAINER_NAME:/app/dist dist/
-# docker rm $CONTAINER_NAME
-
-# Deploy container
-docker build --no-cache -t vilbot -f dockerfile.production .
 
 if docker inspect -f '{{.Name}}' vilbot 2>/dev/null; then
 	if docker inspect -f '{{.State.Running}}' vilbot 2>/dev/null; then
