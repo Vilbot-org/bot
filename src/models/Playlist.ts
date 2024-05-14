b9a6e561f0d0adcc983f1f1294217bc1d12d5c1e
GFrancV <gfrancv@hotmail.com>
2024-05-14 10:31:36 +0100

    refactor: Update Playlist model to use TypeScript

--- src/models/Playlist.ts
+++ src/models/Playlist.ts
@@ -1,5 +1,5 @@
 import { Schema, model } from 'mongoose';
-import type { IPlaylist } from '@/interfaces/IPlaylist';
+import { IPlaylist } from '@/types/IPlaylist';
 
 const playlistSchema: Schema<IPlaylist> = new Schema(
 	{
