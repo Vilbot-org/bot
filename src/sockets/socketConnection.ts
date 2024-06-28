2e8ca9267107b72d1776c36a1c761529a0f33168
GFrancV <gfrancv@hotmail.com>
2024-06-28 11:28:19 +0100

    refactor: Update socketConnection.ts to include authorization token in socket connection headers

--- src/sockets/socketConnection.ts
+++ src/sockets/socketConnection.ts
@@ -3,9 +3,12 @@ import { io } from 'socket.io-client';
 import env from '@/utils/validEnv';
 
 const socketConnection = () => {
-	const token = sign({ userId: 'Vilbot' }, env.JWT_SECRET_KEY);
+	const token = sign({ name: 'Vilbot' }, env.JWT_SECRET_KEY);
 
 	const socket = io(env.SOCKET_URL, {
+		extraHeaders: {
+			authorization: token
+		},
 		auth: {
 			token
 		}
