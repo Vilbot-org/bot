70a2e45aee603c04bfb38892d24e701708944b06
GFrancV <gfrancv@hotmail.com>
2024-08-16 14:39:38 +0100

    refactor: Update play.ts to include buildEmbedDescription function for better track description formatting

--- src/commands/music/queue.ts
+++ src/commands/music/queue.ts
@@ -1,34 +1,56 @@
-import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
+import {
+	ChatInputCommandInteraction,
+	EmbedBuilder,
+	formatEmoji
+} from 'discord.js';
 
 import config from '@/app.config';
 import Command from '@/classes/Command';
 import { getVoiceChannel } from '@/utils/guildUtils';
 import { getQueue } from '@/utils/musicUtils';
+import {
+	formatDurationQueueForEmbed,
+	formatTrackTitleForEmbed
+} from '@/utils/interactions';
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
 	const voiceChannel = getVoiceChannel(interaction);
 
 	const queue = getQueue(voiceChannel);
+	const { currentTrack } = queue;
 
-	const embedMsg = new EmbedBuilder()
-		.setAuthor({ name: 'Music queue!' })
-		.setTitle('Current song:')
-		.setColor(config.colors.success)
-		.setThumbnail(queue.currentTrack?.thumbnail ?? null)
-		.setDescription(
-			`[${queue?.currentTrack?.title}](${queue?.currentTrack?.url})`
-		)
-		.setFooter({ text: `There are ${queue.getSize()} songs in the queue` });
+	let embedDescription = '';
+	if (currentTrack) {
+		embedDescription = `
+    Now playing
+    ${formatEmoji('1273955433299705877')} ${formatTrackTitleForEmbed(
+			currentTrack
+		)} 
+    Requested by <@${currentTrack?.requestedBy?.id}>`;
+	}
 
 	if (!queue.isEmpty()) {
-		embedMsg.addFields(
-			queue.tracks.map((track, key) => ({
-				name: `${key + 1}. ${track.title}`,
-				value: '\u200B',
-				inline: true
-			}))
-		);
+		let tracksStringList = '';
+		queue.tracks.toArray().forEach((track, key) => {
+			tracksStringList += `${key + 1}. ${formatTrackTitleForEmbed(track)} 
+      Requested by <@${track.requestedBy?.id}>\n`;
+		});
+
+		embedDescription += `
+    \n**Tracks in queue**
+    ${tracksStringList}`;
 	}
+
+	const embedMsg = new EmbedBuilder()
+		.setAuthor({ name: 'Music queue' })
+		.setColor(config.colors.success)
+		.setThumbnail(queue.currentTrack?.thumbnail ?? null)
+		.setDescription(embedDescription)
+		.setFooter({
+			text: `${queue.getSize()} songs in the queue - Estimated duration: ${formatDurationQueueForEmbed(
+				queue
+			)}`
+		});
 
 	await interaction.reply({
 		embeds: [embedMsg]
