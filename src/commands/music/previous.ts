f9a810d3ac2182ef7c683fc7a488227605ef7255
GFrancV <gfrancv@hotmail.com>
2024-10-03 12:33:49 +0100

    refactor: Revamp previous command embed to include current track details and user info, improving overall user experience

--- src/commands/music/previous.ts
+++ src/commands/music/previous.ts
@@ -4,6 +4,8 @@ import config from '@/app.config';
 import Command from '@/classes/Command';
 import { previous } from '@/utils/musicUtils';
 import { getVoiceChannel } from '@/utils/guildUtils';
+import { getEmoji } from '@/common/utils/EmojiHelper';
+import { formatTrackTitleForEmbed } from '@/utils/interactions';
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
 	const voiceChannel = getVoiceChannel(interaction);
@@ -11,28 +13,38 @@ const execute = async (interaction: ChatInputCommandInteraction) => {
 	const queue = await previous(voiceChannel);
 
 	const currentTrack = queue.currentTrack;
-	const nextTrack = queue.tracks.at(0);
+
+	const embedDescription = `${getEmoji('previous')} ${formatTrackTitleForEmbed(
+		currentTrack
+	)}`;
+	const embedFooter =
+		queue.getSize() >= 1
+			? `${queue.getSize()} tracks in the queue`
+			: 'No more trakcs in the queue';
 
 	await interaction.reply({
 		embeds: [
 			new EmbedBuilder()
 				.setColor(config.colors.success)
-				.setAuthor({ name: 'Going back to the previous song' })
+				.setAuthor({
+					name: interaction.user.username,
+					iconURL: interaction.user.displayAvatarURL()
+				})
 				.setThumbnail(currentTrack?.thumbnail ?? null)
-				.setTitle(`:track_previous: ${currentTrack?.title}`)
-				.setURL(`${currentTrack?.url}`)
 				.setDescription(
-					queue.getSize() > 1
-						? `The next song is:  [${nextTrack?.title}](${nextTrack?.url})`
-						: 'There are no more songs in the queue!'
+					`**Previous track**
+          ${embedDescription}`
 				)
+				.setFooter({
+					text: embedFooter
+				})
 		]
 	});
 };
 
 const previousCommandOptions = {
 	name: 'previous',
-	description: 'Go back to the previous song!',
+	description: 'Go back to the previous track!',
 	execute: execute
 };
 const previousCommand = new Command(previousCommandOptions);
