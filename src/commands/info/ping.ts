12a05d942d7d70ba93c49da358aa02270e216bc6
GFrancV <gfrancv@hotmail.com>
2024-10-03 11:36:02 +0100

    refactor: Update ping command embed for improved styling and enhanced clarity in displayed ping information

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -2,16 +2,16 @@ import { EmbedBuilder } from '@discordjs/builders';
 import { ChatInputCommandInteraction } from 'discord.js';
 
 import Command from '@/classes/Command';
+import appConfig from '@/app.config';
 
 const execute = async (interaction: ChatInputCommandInteraction) => {
 	await interaction.reply({
 		embeds: [
-			new EmbedBuilder()
-				.setColor(0x01feff)
-				.setTitle('Pong!')
-				.setDescription(
-					`:ping_pong: The ping of the bot are \`${interaction.client.ws.ping}ms\``
-				)
+			new EmbedBuilder().setColor(appConfig.colors.info).setDescription(
+				`
+          **Ping**
+          :ping_pong: Ping \`${interaction.client.ws.ping}ms\``
+			)
 		],
 		ephemeral: true
 	});
