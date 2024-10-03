e7727f5c8bcfb187d8e53612576da8ad864b211c
GFrancV <gfrancv@hotmail.com>
2024-10-03 15:17:54 +0100

    refactor: Update info color in app config for improved visibility and enhance overall color scheme consistency

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,10 +1,8 @@
 export default {
-	botName: 'Vilbot',
-	author: 'GFrancV',
 	colors: {
 		success: 0x01feff,
 		danger: 0xff0201,
-		info: 0x8101ff,
+		info: 0xe879f9,
 		green: 0x7fff01
 	},
 	siteURL: 'https://vilbot-org.github.io'
